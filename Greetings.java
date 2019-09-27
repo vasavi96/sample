27dcb33175b2a8ceb3564e3ef1ded5f8e57a66fe
Jajula Vasavi <jajula.vasavi@intellectdesign.com>
2019-09-27 12:47:19 +0530

    modified the file

--- Greetings.java
+++ Greetings.java
@@ -2,5 +2,6 @@
 class Greetings{
 	public static void main(String[] ar) {
 		System.out.println("Welcome to git..");
+		System.out.println("working on git & testing how to commit and push my changes:");
     }
 }
