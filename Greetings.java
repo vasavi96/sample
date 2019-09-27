192084d9fa6c637a67b5debddef9c782c929b357
Jajula Vasavi <jajula.vasavi@intellectdesign.com>
2019-09-27 13:12:08 +0530

    made changes in brn

--- Greetings.java
+++ Greetings.java
@@ -3,5 +3,6 @@ class Greetings{
 	public static void main(String[] ar) {
 		System.out.println("Welcome to git..");
 		System.out.println("working on git & testing how to commit and push my changes:");
+		System.out.println("working on new branch");
     }
 }
